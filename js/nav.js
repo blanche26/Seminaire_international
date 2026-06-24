8490e514fd66692eee178172bbeac287749c844d
blanche26 <lystolepadiolekamaleulatale@gmail.com>
2026-06-24 10:27:55 +0100

    Create nav.js

---
+++ js/nav.js
@@ -0,0 +1,10 @@
+/**
+ * Module de navigation (nav.js)
+ * Gère les comportements du menu et de la navigation
+ */
+
+function initMobileNav() {
+    // Sécurise l'appel fait dans app.js pour éviter les erreurs de fonction manquante
+    console.log("Navigation initialisée.");
+}
+
