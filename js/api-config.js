cefdd22c626115d08fbcff3499a67835d168156c
blanche26 <lystolepadiolekamaleulatale@gmail.com>
2026-06-24 10:23:37 +0100

    Create api-config.js

---
+++ js/api-config.js
@@ -0,0 +1,10 @@
+/**
+ * Configuration globale de l'API REST (MockAPI)
+ */
+
+// L'URL de base de ton serveur
+const BASE_URL = "https://6a3676af766b831960f941b8.mockapi.io";
+
+// L'endpoint exact pour accéder à tes participants
+const API_URL = BASE_URL + "/participants";
+
